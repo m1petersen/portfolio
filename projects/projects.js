58c28f4aeb667dd6de1c909afada99d90c1b113a
Michael Petersen <m1petersen@ucsd.edu>
2026-04-28 17:28:40 -0700

    Lab 5 Changes

--- projects/projects.js
+++ projects/projects.js
@@ -1,17 +1,82 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-// Fetch the project data from the JSON file
 const projects = await fetchJSON('../lib/projects.json');
-
-// Select the container where the projects will be rendered
 const projectsContainer = document.querySelector('.projects');
-
-// Render the projects into the container
-renderProjects(projects, projectsContainer, 'h2');
-
 const projectsTitle = document.querySelector('.projects-title');
 
-// Check if the element exists so we don't get errors
 if (projectsTitle) {
     projectsTitle.textContent = `${projects.length} Projects`;
 }
+
+let query = '';
+let selectedYear = null;
+
+function renderPieChart(projectsGiven) {
+  let svg = d3.select('#projects-pie-plot');
+  svg.selectAll('path').remove();
+  let legend = d3.select('.legend');
+  legend.html('');
+
+  let rolledData = d3.rollups(
+    projectsGiven,
+    (v) => v.length,
+    (d) => d.year,
+  );
+
+  let data = rolledData.map(([year, count]) => {
+    return { value: count, label: year };
+  });
+
+  let colors = d3.scaleOrdinal(d3.schemeTableau10);
+  let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+  let sliceGenerator = d3.pie().value((d) => d.value);
+  let arcData = sliceGenerator(data);
+  let arcs = arcData.map((d) => arcGenerator(d));
+
+  arcs.forEach((arc, i) => {
+    svg.append('path')
+       .attr('d', arc)
+       .attr('fill', colors(i))
+       .attr('class', () => (data[i].label === selectedYear ? 'selected' : ''))
+       .on('click', () => {
+         selectedYear = selectedYear === data[i].label ? null : data[i].label;
+         setFilters();
+       });
+  });
+
+  data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`)
+          .attr('class', () => (d.label === selectedYear ? 'legend-item selected' : 'legend-item'))
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+  });
+}
+
+function setFilters() {
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    let matchesQuery = values.includes(query.toLowerCase());
+    let matchesYear = selectedYear ? project.year === selectedYear : true;
+    return matchesQuery && matchesYear;
+  });
+
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+
+  let pieChartData = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+
+  renderPieChart(pieChartData);
+}
+
+let searchInput = document.querySelector('.searchBar');
+if (searchInput) {
+  searchInput.addEventListener('input', (event) => {
+    query = event.target.value;
+    setFilters();
+  });
+}
+
+setFilters();
